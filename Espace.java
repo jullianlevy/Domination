f055738d22d9d3151666f0e4c605475eca07cd65
jullianlevy <44406607+jullianlevy@users.noreply.github.com>
2019-01-07 09:57:49 +0100

    Update Espace.java

--- Espace.java
+++ Espace.java
@@ -5,7 +5,20 @@ import java.util.List;
 public class Espace {
 	static int largeur=9;
 	static int longueur=9;
+	Case[][] grille=new Case[largeur][hauteur];
 	
+	
+	public Espace(){
+		for(int i=0;i<longueur;i++){
+			for(int j=0;j<largeur;j++){
+				grille[i][j]=new Case(i,j);
+			}
+		}
+		chateau=grille[4][4];
+		chateau.setTerrain(Type.Terrain.CHATEAU);
+		chateau.setNbCouronnes(0);
+	}
+			
 	
 	
 	
