406587ac722f2a5c4925595bf9d3923488de6270
paul92isep <45355270+paul92isep@users.noreply.github.com>
2019-01-09 17:09:55 +0100

    Update Joueurs.java

--- Joueurs.java
+++ Joueurs.java
@@ -1,70 +1,58 @@
 import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Joueurs {
 
-	private String name;
-	int nbrjoueurs;
+	public String name;
+	static int nbrjoueurs;
 	private int score;
-	ArrayList<Rois> listrois=new ArrayList<Rois>();
-	ArrayList<Domino> dominojoueurs=new ArrayList<Domino>();
-	
+	private int id;
+
+	ArrayList<Rois> listrois = new ArrayList<Rois>();
+	ArrayList<Domino> dominojoueurs = new ArrayList<Domino>();
+
 	Espace plateau;
-	
 
-	
-	
-	
-	
-	public Joueurs(String name) {
-		this.name=name;
+	public Joueurs(String name, int id) {
+		this.name = name;
+		this.id = id;
 
-
-	
-}
-	
-	
-
-
-public void NombreJoueurs() {
-	Scanner nbjoueur=new Scanner(System.in);
-	System.out.println("Indiquer le nombre de joueur: ? ");
-	while(!nbjoueur.hasNext("[2-4]")) {
-		System.out.println("Entrer un nombre entre 2 et 4");
-	nbjoueur.next();
 	}
-	nbrjoueurs=nbjoueur.nextInt();
-	
-}
 
+	public void NombreJoueurs() {
+		Scanner nbjoueur = new Scanner(System.in);
+		System.out.println("Indiquer le nombre de joueur: ? ");
+		while (!nbjoueur.hasNext("[2-4]")) {
+			System.out.println("Entrer un nombre entre 2 et 4");
+			nbjoueur.next();
+		}
+		nbrjoueurs = nbjoueur.nextInt();
 
- 
-
+	}
 
 	public String getName() {
 		return name;
 	}
 
-
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public int getScore() {
 		return score;
 	}
 
-
-
-
 	public void setScore(int score) {
 		this.score = score;
 	}
 
+	public int getId() {
+		return id;
+	}
 
-
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
