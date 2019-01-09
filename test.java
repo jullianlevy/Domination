3a6768b179362a869434eb92ec17dd3bc43a5090
paul92isep <45355270+paul92isep@users.noreply.github.com>
2019-01-09 17:10:37 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,66 +1,51 @@
 import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.scene.paint.Color;
+
+import java.util.Random;
+import java.util.Collections;
+import java.util.Comparator;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class test {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
+
 		System.out.println("--------------------------------------");
 		System.out.println("BIENVENUE DOMIN'ATION");
 		System.out.println("--------------------------------------");
 		System.out.println("");
-		
-	
-		Deroulement.creationJoueur();
-		Domino.creationliste();
-		Deroulement.ChoisirRois();
-		
-	
-		for(Joueurs joueur:Joueurs.listjoueurs) {
-			for(Rois roi:joueur.listrois) {
-				System.out.println(roi.getColorRoi()+"  "+joueur.getName());
-			}
-		}
-		
 
-			
-		
-	
-		
+		Deroulement der = new Deroulement();
+
+		// Deroulement.ChoisirRois();
+
 	}
 
-			
-			
-		
-		
-			
-			
-		
-		//Domino.creationliste();
-		//Deroulement.NombreDomino();
-		//Deroulement.Melangerdom();
-		//Deroulement.ChoisirRois();
-		//for(Joueurs joueur:Joueurs.listjoueurs) {
-			//System.out.println(joueur.getName()+joueur.listrois);
-		//}
-	
-		
+	// Domino.creationliste();
+	// Deroulement.NombreDomino();
+	// Deroulement.Melangerdom();
+	// Deroulement.ChoisirRois();
+	// for(Joueurs joueur:Joueurs.listjoueurs) {
+	// System.out.println(joueur.getName()+joueur.listrois);
+	// }
 
-	
-	
-	
-
-	
-	
 	public static void NombreJoueurs() {
-		Scanner nbjoueur=new Scanner(System.in);
+		Scanner nbjoueur = new Scanner(System.in);
 		System.out.println("Indiquer le nombre de joueur: ");
-		while(!nbjoueur.hasNext("[2-4]")) {
+		while (!nbjoueur.hasNext("[2-4]")) {
 			System.out.println("Entrer un nombre entre 2 et 4");
-		
-		nbjoueur.nextInt();
+
+			nbjoueur.nextInt();
 		}
-		Joueurs.nbrjoueurs=nbjoueur.nextInt();
-		
+		Joueurs.nbrjoueurs = nbjoueur.nextInt();
+
 	}
+
 }
